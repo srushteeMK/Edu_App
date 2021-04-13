047bc66b791fd8de0f3239440af988c8f801749d
srushteeMK <76172199+srushteeMK@users.noreply.github.com>
2021-04-14 05:00:33 +0530

    Update NotificationsScreen.js

--- screens/NotificationsScreen.js
+++ screens/NotificationsScreen.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,StyleSheet } from 'react-native';
+import db from "../config";
+import firebase from "firebase";
 
+import MyHeader from "../components/MyHeader";
+import CustomButton from "../components/CustomButton";
+import CustomInput from "../components/CustomInput";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 class NotificationsScreen extends Component {
   constructor(props) {
     super(props);
@@ -10,11 +16,37 @@ class NotificationsScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text> NotificationsScreen </Text>
-      </View>
+      <SafeAreaProvider>
+      <View style={styles.container}>
+        <MyHeader title="Notifications" navigation={this.props.navigation} />
+        </View>
+        </SafeAreaProvider>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  upperContainer: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center"
+  },
+  input: {
+    width: "75%",
+    height: 55,
+    borderColor: "#6fc0b8",
+    marginTop: 20
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#6fc0b8"
+  },
+  buttonTitle: {
+    color: "#fff"
+  }
+});
+
 
 export default NotificationsScreen;
